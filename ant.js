ef071ac9c70f1a3bfa846ff429321aba17a85979
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2020-09-20 15:04:57 -0700

    Update and rename TP_Ant_10101.js to ant.js
    Renamed the fillCurrentSquare() function to updateSquare(). I think that's better in my opinion. Also, I'll be updating the .html file to reflect renaming this file.

--- ant.js
+++ ant.js
@@ -1,21 +1,27 @@
 /* Author: Anthony Sam (anthonysam538@csu.fullerton.edu)
 This is the javascript file. While the .html file contains the programming 
-for the html page, this .js file contains the programming for the ant. The 
-setup() function will automatically be called at the start, and the draw() 
-function will be called on every frame. The keyPressed() function will be 
-called when a key on the keyboard is pressed and the mousePressed() 
-function will be called when the user clicks the mouse. fillCurrentSquare() 
-is a function that can be called from within other functions. 
-In this case, it's called within draw() and mousePressed(). This .js file 
-uses a 2-D array that contains numbers associated with each of the 5 
-colors. By checking the number of a selected spot in the array, the ant is 
-able to determine what color it is on so that it can change direction and 
-change the tile's color. */
+for the html page, this .js file contains the programming for the ant.
+
+The setup() function will automatically be called at the start, and the 
+draw() function will be called on every frame. The keyPressed() function 
+will be called when a key on the keyboard is pressed and the 
+mousePressed() function will be called when the user clicks the mouse. 
+updateSquare() is a function that can be called from within other 
+functions. You could call the other functions like setup() and 
+mousePressed() from other functions too, but that's typically not too 
+useful. Anyways, we can see that updateSquare() is called within 
+draw() and mousePressed(). By doing this, the number of lines of code goes 
+down a bit, and maybe the code's more readable too.
+
+Finally, this .js file uses a 2-D array that contains numbers associated 
+with each of the 5 colors. By checking the number of a selected spot in 
+the array, the ant is able to determine what color it is on so that it can 
+change direction and change the tile's color. */
 
 function setup() // p5.js setup function
 {
     /* Create a JavaScript object named "grid." This will contain handy information about the grid of cells.
-    cell_size: The size, in pixels, of each cell
+    cell_size: The size of each cell, in pixels
     width: The amount of cells in a row
     height: The amount of cells in a column
     colors: A 2-D array that contains the colors for all cells in the grid, stored as a number. */
@@ -25,7 +31,7 @@ function setup() // p5.js setup function
 
     frameRate(24); // draw() will be called 24 times per second
 
-    // Initialize the grid
+    // Initialize the grid (Note that the variables x and y here are declared as local variables, so they'll disappear when their for loops end)
     for (let x = 0; x < grid.width; ++x)
     {
         grid.colors[x] = []; // create nested array
@@ -55,11 +61,11 @@ function draw() // p5.js draw function
     }
 
     // Increment the color of the tile that the bot's standing on
-    grid.colors[bot.x][bot.y] = ++grid.colors[bot.x][bot.y] % 5; // change the value in the 2-D array
-    fillCurrentSquare();
+    grid.colors[bot.x][bot.y] = ++grid.colors[bot.x][bot.y] % 5; // first, update the value in the 2-D array
+    updateSquare();
     
-    // Update the bot's coordinates by using its current coordinates and its new direction
-    fill('white');
+    // Update the bot's coordinates, then redraw the bot
+    fill('white'); // change the color we're currently using to white for the bot
     switch(bot.direction)
     {
         case 0: // facing north
@@ -87,6 +93,7 @@ function draw() // p5.js draw function
     }
 }
 
+// When a key on the keyboard is pressed, pause the bot
 function keyPressed()
 {
     if (isLooping()) // if the program is running, then pause the program
@@ -95,12 +102,13 @@ function keyPressed()
         loop();
 }
 
+// If the mouse clicked in the canvas, then relocate the bot to where the mouse clicked
 function mousePressed()
 {
     // console.log("Mouse (x,y): (" + x_position + ',' + y_position + ')');
 
     // First, remove the bot from where it currently is by drawing the tile it's standing on over the bot
-    fillCurrentSquare();
+    updateSquare();
     
     // Then, move the bot to where the mouse clicked
     if (mouseX >= 0 && mouseX < width && mouseY >= 0 && mouseY < height) // if the user clicked in the canvas
@@ -110,8 +118,9 @@ function mousePressed()
     }
 }
 
-// This code snippet was used twice, so I thought I'd put it in a function
-function fillCurrentSquare()
+// This code snippet was used twice, so I thought I'd put it in a function.
+// This function uses the number found in grid.colors[][] to determine what color the square should now be
+function updateSquare()
 {
     switch (grid.colors[bot.x][bot.y]) // change the color we are currently using
     {
